1656b1638602ad4e5ef643991cc87de9f63660f5
lennymercadovaldez <71667982+lennymercadovaldez@users.noreply.github.com>
2024-01-27 13:51:03 -0500

    Update API URLs and remove unused code

--- API/ServiceWeather.js
+++ API/ServiceWeather.js
@@ -1,13 +1,16 @@
 // ServiceWeather.js
 import axios from 'axios'
-import { API_KEY, WEATHER_API_URL, CITY_API_URL, GEOCODING_API_URL, OPENCAGE_API_KEY, OPENCAGE_API_URL } from './Weather';
 
 export default {
     async getWeatherData(lat, lon) {
         try {
-            const { data: weatherData } = await axios.get(`${WEATHER_API_URL}?location=${lat},${lon}&fields=temperature,humidity,windSpeed&timesteps=1h&units=metric&apikey=${API_KEY}`)
-            const { data: forecastData } = await axios.get(`${WEATHER_API_URL}?location=${lat},${lon}&fields=temperature,humidity,windSpeed&timesteps=1d&units=metric&apikey=${API_KEY}`)
-            const { data: cityData } = await axios.get(`${CITY_API_URL}&lat=${lat}&lon=${lon}`)
+            const { data: weatherData } = await axios.get(`${import.meta.env.VITE_WEATHER_API_URL}?location=${lat},${lon}&fields=temperature,humidity,windSpeed&timesteps=1h&units=metric&apikey=${import.meta.env.VITE_API_KEY}`)
+            const { data: forecastData } = await axios.get(`${import.meta.env.VITE_WEATHER_API_URL}?location=${lat},${lon}&fields=temperature,humidity,windSpeed&timesteps=1d&units=metric&apikey=${import.meta.env.VITE_API_KEY}`)
+            const { data: cityData } = await axios.get(`${import.meta.env.VITE_CITY_API_URL}&lat=${lat}&lon=${lon}`)
+
+            if (!weatherData.data || !forecastData.data || !cityData) {
+                throw new Error('Los datos devueltos por la API no son los esperados')
+            }
 
             const weather = weatherData.data.timelines[0].intervals[0].values
             const forecast = forecastData.data.timelines[0].intervals.map(interval => interval.values)
@@ -21,7 +24,7 @@ export default {
     },
     async getCoordinates(city) {
         try {
-            const url = `${GEOCODING_API_URL}&q=${encodeURIComponent(city)}&limit=1`
+            const url = `${import.meta.env.VITE_GEOCODING_API_URL}&q=${encodeURIComponent(city)}&limit=1`
             console.log(`Making request to: ${url}`)
             const { data } = await axios.get(url)
             const lat = data[0].lat
@@ -34,7 +37,7 @@ export default {
     },
     async getCityInfo(lat, lon) {
         try {
-          const response = await axios.get(`${OPENCAGE_API_URL}?q=${lat}+${lon}&key=${OPENCAGE_API_KEY}`);
+          const response = await axios.get(`${import.meta.env.VITE_OPENCAGE_API_URL}?q=${lat}+${lon}&key=${import.meta.env.VITE_OPENCAGE_API_KEY}`);
           const data = response.data.results[0];
           const formattedAddress = data.formatted;
           const components = data.components;
